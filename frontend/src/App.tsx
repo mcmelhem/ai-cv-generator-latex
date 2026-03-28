eead8882f78c2392551551b0881805420ad840c4
mcmelhem <marieclaire.melhem@gmail.com>
2026-03-28 18:33:49 +0200

    Fix UI - HTML and CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,27 @@
 import './App.css'
 import Form from './components/Form'
+
 function App() {
- 
- 
+
+
   return (
     <>
       <div className="ticks">
-        <h2>Gen CV</h2>
-        <Form/>
+        <nav className="site-header sticky-top py-1">
+          <div className="container d-flex flex-column flex-md-row justify-content-between">           
+              <h5 className="py-2">LaTeXGen CV</h5>
+            <a className="py-2 d-none d-md-inline-block" href="#">Create Cv</a>
+          </div>
+        </nav>
+
+        <div className="headline position-relative overflow-hidden p-3 p-md-5 mx-2 my-1 text-center">
+          <div className="col-md-10 p-lg-5 mx-auto my-5">
+            <h1 className="display-4 font-weight-normal">Generate CV In Latex Format</h1>
+            <p className="lead font-weight-normal">Add Your Personal Info To obtain a CV generated in Latex Format base don Job Description</p>
+            <button className='btn btn-sm btn-start'>Start Now</button>
+          </div>
+        </div>
+        <Form />
       </div>
     </>
   )
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import './Form.css'
 
 function Form() {
   const [jobDescription, setJobDescription] = useState('')
@@ -15,10 +17,11 @@ function Form() {
       experience: '',
       education: '',
       projects: '',
+      jobDescription: ''
     }
   )
-  async function handleGenerate(){
-    
+  async function handleGenerate() {
+
     try {
       const response = await fetch("http://127.0.0.1:5000/generateCV", {
         method: "POST",
@@ -36,120 +39,153 @@ function Form() {
       console.error("Error:", error);
     }
   }
+  function openPanel() {
+    var panel = document.getElementById('info');
+    if (panel) {
+      if (panel.style.display == 'none') {
+        panel.style.display = 'block';
+      } else {
+        panel.style.display = 'none';
+      }
+    }
+
+  }
   return (
     <>
-     
-        <h5>Input A Job Description Below</h5>
-        <div className='container'>
-        <div className='row mb-1'>
-          <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Name" 
-              value={formData.name}
-              onChange={(e) => setFormData({...formData, name: e.target.value})}/>
+
+      <div className='form container-fluid'>
+        <div className='header'>
+        </div>
+        <div className='cv-building row mb-2'>
+          <div className='cv-options col-1 ps-1 pe-0'>
+            <div className='option-item fill-info' onClick={() => openPanel()}>
+              <button title='Add Info' className='btn btn-sm' >
+                <FontAwesomeIcon icon={faAddressCard} />
+              </button>
+              <span className='text-center'>Fill Personal Info </span>
+            </div>
+            <div className='option-item import-cv'>
+              <button title='Import cv' className='btn btn-sm'>
+                <FontAwesomeIcon icon={faUser} />
+              </button>
+              <span className='text-center'>Import Existing CV</span>
+            </div>
           </div>
-            <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Email" 
-              value={formData.email}
-              onChange={(e) => setFormData({...formData, email: e.target.value})}/>
+          <div className='col-11'>
+            <div className='collapsible-info p-2' id='info' style={{ display: 'none' }}>
+              <div className='row mb-1'>
+                <div className='col'>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    value={formData.name}
+                    onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+                </div>
+                <div className='col'>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                </div>
+              </div>
+              <div className='row mb-1'>
+                <div className='col'>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Phone"
+                    value={formData.phone}
+                    onChange={(e) => setFormData({ ...formData, phone: e.target.value })} />
+                </div>
+                <div className='col'>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Linkedin"
+                    value={formData.linkedin}
+                    onChange={(e) => setFormData({ ...formData, linkedin: e.target.value })} />
+                </div>
+              </div>
+
+              <div className='row mb-1'>
+                <div className='col-6'>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Github"
+                    value={formData.github}
+                    onChange={(e) => setFormData({ ...formData, github: e.target.value })} />
+                </div>
+
+              </div>
+              <div className='row mb-1'>
+                <div className='col'>
+                  <textarea
+                    className="form-control"
+                    placeholder="Skills"
+                    value={formData.skills}
+                    onChange={(e) => setFormData({ ...formData, skills: e.target.value })} />
+                </div>
+                <div className='col'>
+                  <textarea
+                    className="form-control"
+                    placeholder="Experience"
+                    value={formData.experience}
+                    onChange={(e) => setFormData({ ...formData, experience: e.target.value })} />
+                </div>
+                <div className='col'>
+                  <textarea
+                    className="form-control"
+                    placeholder="Education"
+                    value={formData.education}
+                    onChange={(e) => setFormData({ ...formData, education: e.target.value })} />
+                </div>
+              </div>
+              <div className='row mb-1'>
+                <div className='col'>
+                  <textarea
+                    className="form-control"
+                    placeholder="Projects"
+                    value={formData.projects}
+                    onChange={(e) => setFormData({ ...formData, projects: e.target.value })} />
+                </div>
+              </div>
+            </div>
+            <textarea
+              name="job-description"
+              className="job-description form-control"
+              id="job-description"
+              placeholder='Job Description...'
+              cols={30}
+              rows={8}
+              value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
+            ></textarea>
           </div>
         </div>
-        <div className='row mb-1'>
-          <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Phone" 
-              value={formData.phone}
-              onChange={(e) => setFormData({...formData, phone: e.target.value})}/>
-          </div>
-            <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Linkedin" 
-              value={formData.linkedin}
-              onChange={(e) => setFormData({...formData, linkedin: e.target.value})}/>
-          </div>
+        <div className='generate'>
+          <button className="btn btn-secondary mx-auto" onClick={() => handleGenerate()}>Generate Latex</button>
         </div>
-        
-        <div className='row mb-1'>
-          <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Github" 
-              value={formData.github}
-              onChange={(e) => setFormData({...formData, github: e.target.value})}/>
-          </div>
-            <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Linkedin" 
-              value={formData.skills}
-              onChange={(e) => setFormData({...formData, skills: e.target.value})}/>
-          </div>
-        </div>
-        <div className='row mb-1'>
-          <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Experience" 
-              value={formData.experience}
-              onChange={(e) => setFormData({...formData, experience: e.target.value})}/>
-          </div>
-          <div className='col'>
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Education" 
-              value={formData.education}
-              onChange={(e) => setFormData({...formData, education: e.target.value})}/>
-          </div>
-        </div>
-         <div className='row mb-1'>
-          <div className='col'>
-            <textarea 
-              className="form-control" 
-              placeholder="Projects" 
-              value={formData.projects}
-              onChange={(e) => setFormData({...formData, projects: e.target.value})}/>
-          </div>
-       
-        </div>
-        <textarea 
-          name="job-description" 
-          className="job-description form-control"
-          id="job-description" 
-          placeholder='Job Description...'
-          cols={30} 
-          rows={10} 
-          value={jobDescription}
-          onChange={(e) => setJobDescription(e.target.value)}
-        ></textarea>
-       <button className="btn btn-secondary mt-1" onClick={()=> handleGenerate()}>Generate Latex</button>
       </div>
-      <div className="result">
-        <h2>Generated LaTeX CV</h2>
-        <textarea 
-          name="cv-result" 
+
+      <div className="result" style={{ display: 'none' }}>
+
+        <textarea
+        
+          name="cv-result"
           className="cv-result form-control"
-          id="cv-result" 
+          id="cv-result"
           placeholder='Generated CV LaTeX will appear here...'
-          cols={30} 
-          rows={20} 
+          cols={30}
+          rows={20}
           value={cvResult}
           readOnly
         ></textarea>
-         <button className="btn btn-secondary mt-1">Copy</button>
-     </div>
+        <button className="btn btn-secondary mt-1">Copy</button>
+      </div>
     </>
   )
 }
